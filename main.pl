525360ea0c01b5ab75ebec8c7d2817d647674fc2
shane <sdrafahl@iastate.edu>
2018-02-04 17:23:48 -0600

    Can now parse json string returned from coin exchange

--- main.pl
+++ main.pl
@@ -1,9 +1,14 @@
 #!/usr/bin/perl -w
 
-use LWP::Simple;
+use LWP::UserAgent();
+use JSON::XS;
+
+my $ua = LWP::UserAgent->new;
+$ua->timeout(10);
+$ua->env_proxy;
 
 sub say {print @_, "\n"}
 
-$data = get("https://api.coinmarketcap.com/v1/ticker/neblio/");
+$data = $ua->get("https://api.coinmarketcap.com/v1/ticker/neblio/");
 
-say $data;
+say JSON::XS::decode_json($data->decoded_content)->[0]->{'id'};
