0fbb117cdc8d0a5961cb8a29a00670dc67cedb1d
shane <sdrafahl@iastate.edu>
2018-02-10 15:47:09 -0600

    Added time

--- main.pl
+++ main.pl
@@ -2,8 +2,13 @@
 
 use CryptoConnection "/CryptoConnection.pm";
 
+use POSIX qw/strftime/;
+
 sub say {print @_, "\n"}
 
 $neblioConnection = new CryptoConnection("Neblio");
 
-say "Price USD: " . $neblioConnection->getPrice();
+while (1) {
+    sleep(2);
+    say strftime('%Y-%m-%d %R',localtime) . " Price USD: " . $neblioConnection->getPrice();
+}
